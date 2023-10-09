787dede262552b6d19336cb918a3081c8dd0cb5f
Jakub Svoboda <132791205+jacobfreedom@users.noreply.github.com>
2023-10-09 14:49:38 +0200

    Color Customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,23 @@
 import autoprefixer from 'autoprefixer'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+
 import React from "react";
-import {Button, Checkbox} from "@nextui-org/react";
+
+import {Button, Checkbox, Pagination, 
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem,
+  RadioGroup, 
+  Radio
+} from "@nextui-org/react";
 
 export default function Home() {
-  const [isSelected, setIsSelected] = React.useState(false);
+
+  const [selectedColor, setSelectedColor] = React.useState("primary")
+
 
   return (
     <main className={styles.container}>
@@ -35,6 +47,21 @@ export default function Home() {
             <Image src="/Tailwind.png" alt='Tailwind logo' layout='fill' className={'image'}></Image>
           </div>
         </div>
+        <div className=''>
+          <RadioGroup
+                label="Select your color"
+                orientation="horizontal"
+                color={selectedColor}
+                defaultValue="primary"
+                onValueChange={setSelectedColor}
+              >
+                <Radio value="primary">Primary</Radio>
+                <Radio value="secondary">Secondary</Radio>
+                <Radio value="success">Success</Radio>
+                <Radio value="warning">Warning</Radio>
+                <Radio value="danger">Danger</Radio>
+              </RadioGroup>
+          </div>
       </div>
 
       <div className={styles.todo__interface}>
@@ -49,37 +76,22 @@ export default function Home() {
             </div>
         </div>
 
-        <div className={styles.todo_items}>
-
-          <div className={styles.statuscheck}>
-            <div className="flex flex-col">
-              <Checkbox color="success" radius="full" isSelected={isSelected} onValueChange={setIsSelected}>
-                Subscribe
-              </Checkbox>
-              <p className="text-default-500">
-                Selected: {isSelected ? "true" : "false"}
-              </p>
-              <Checkbox color="success">Success</Checkbox>
-            </div>
-          </div>
+        <div className={styles.todo__items}>
 
 
+          {/* first item */}
           <div className={styles.todo__item__elements}>
+
+            <div className="flex items-center">
+              <Checkbox color={selectedColor} radius="full" />
+            </div>
             <div className={styles.todo__elements__cotent}>
               <div className='font-semibold'>
-                Nakoupit si rohliky
+                An example To Do Title
               </div>
 
-              <div className='font-extralight text-sm'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius enim sed orci pellentesque, ut ornare justo vulputate. Cras tristique ante ut mauris sagittis, sit amet volutpat justo aliquet. Pellentesque pulvinar eleifend dignissim. Proin fringilla massa lorem, ut facilisis neque posuere ac. Maecenas sollicitudin, sem eget porttitor placerat, elit dui semper dolor, non lacinia felis neque sit amet massa. Cras nec urna vestibulum, malesuada est eget, eleifend erat. Ut sed erat dapibus, tristique urna sed, ultrices lacus. Mauris lobortis sem et finibus convallis. Quisque malesuada magna eget diam fermentum tristique. Etiam lobortis, libero ac euismod lacinia, enim leo auctor eros, non hendrerit ligula ex non mauris. Praesent vitae pretium mauris, a auctor metus.
-
-              Ut metus tortor, finibus vitae mauris vitae, tempor porttitor erat. Suspendisse sed magna vestibulum, sodales libero at, eleifend orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus luctus risus sed maximus elementum. Morbi nec risus lectus. Cras suscipit faucibus imperdiet. Sed nec urna enim. Ut dapibus iaculis nisi vel consequat. Aenean pharetra sollicitudin augue, ac sodales lacus facilisis in.
-
-              Sed finibus, est sit amet efficitur ultrices, neque purus pharetra mauris, vel suscipit orci quam vel tellus. Phasellus vitae mi id magna egestas ornare. Nullam vitae varius mauris. Praesent tempus iaculis nisl, eu congue felis. Fusce justo metus, egestas vitae urna nec, malesuada aliquet mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse auctor nibh nec nulla convallis iaculis. Sed sapien enim, consectetur at sem eget, faucibus eleifend nulla. Phasellus eget eros laoreet, mattis tortor in, viverra nisl. Sed sit amet imperdiet ante. Vestibulum eu diam vel sem viverra sodales quis a diam. Nulla consequat consequat consequat. In ultricies sit amet lectus a bibendum. Aenean ac convallis enim, et tincidunt purus. Mauris a turpis venenatis diam suscipit interdum. Vestibulum blandit, mi euismod suscipit facilisis, orci purus vehicula lorem, placerat dictum tortor sem eget justo.
-
-              Nulla a maximus turpis. Integer faucibus est at lectus convallis, et dictum nisi varius. Morbi ac diam ut felis commodo tincidunt. Donec felis sapien, vehicula sed cursus vitae, tincidunt eget mauris. Curabitur et enim a ex porttitor ultrices in sit amet turpis. Sed aliquam quis justo et varius. Proin consequat mi vitae arcu tempus, ut vehicula ligula scelerisque. Cras eleifend velit eget dui cursus, vitae pulvinar erat sagittis. Nulla malesuada elit id erat eleifend, fringilla vulputate urna consequat. Sed in eros id lacus dignissim luctus. Nullam viverra maximus ligula id gravida. In tincidunt massa a magna bibendum aliquam.
-
-              Integer malesuada orci nisi, quis rhoncus ligula venenatis at. In egestas lorem elit, finibus dapibus erat pharetra id. Aliquam blandit, mauris sed auctor dapibus, felis nibh feugiat ipsum, ut varius velit diam vel augue. Suspendisse convallis tortor massa, ut suscipit nunc porttitor id. Pellentesque non finibus orci. Cras ornare varius urna ut lobortis. Duis at libero vitae ante efficitur sodales. Vivamus vulputate justo quis sapien suscipit, at euismod risus euismod. Duis interdum mi at libero scelerisque fringilla. Pellentesque luctus ut ante ut iaculis. Suspendisse potenti. Morbi efficitur mi est, id accumsan lacus tincidunt at. Duis vitae nunc in ante ultricies malesuada id et neque.
+              <div className='font-extralight text-sm h-13'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius enim sed orci pellentesque, ut ornare justo vulputate. Cras tristique ante ut mauris sagittis, sit amet volutpat justo aliquet. Pellentesque pulvinar eleifend dignissim. 
               </div>
             </div>
 
@@ -87,11 +99,65 @@ export default function Home() {
               <div className='font-medium'>
                 Deadline
               </div>
-              <div className='font-extralight'>
+              <div className='font-extralight truncate'>
+                01. 13. 2028
+              </div>
+            </div>  
+            <div className="flex">
+              <Dropdown>
+                <DropdownTrigger>
+                  <Button 
+                    variant="light"
+                    color={selectedColor} 
+                  >
+                    Open Menu
+                  </Button>
+                </DropdownTrigger>
+                <DropdownMenu 
+                  aria-label="Action event example" 
+                  onAction={(key) => alert(key)}
+                >
+                  <DropdownItem key="edit">Edit file</DropdownItem>
+                  <DropdownItem key="delete" className="text-danger" color="danger">
+                    Delete file
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </div>
+          </div>
+
+
+          {/* second item */}
+          <div className={styles.todo__item__elements}>
+
+
+            <div className="flex items-center">
+              <Checkbox color="success" radius="full" />
+            </div>
+            <div className={styles.todo__elements__cotent}>
+              <div className='font-semibold'>
+                An example To Do Title
+              </div>
+
+              <div className='font-extralight text-sm h-13'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius enim sed orci pellentesque, ut ornare justo vulputate. Cras tristique ante ut mauris sagittis, sit amet volutpat justo aliquet. Pellentesque pulvinar eleifend dignissim. 
+              </div>
+            </div>
+
+            <div className={styles.todo__elements__info}>
+              <div className='font-medium'>
+                Deadline
+              </div>
+              <div className='font-extralight truncate'>
                 01. 13. 2028
               </div>
             </div>  
           </div>
+          
+        </div>
+
+        <div className='flex items-center justify-center m-3'>
+          <Pagination loop showControls color={selectedColor} total={5} initialPage={1} />
         </div>
       </div>
 
