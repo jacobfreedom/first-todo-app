062b4a09a3399ab727bbfb4a8dbc45e5778f86a6
Jakub Svoboda <132791205+jacobfreedom@users.noreply.github.com>
2023-11-30 23:24:13 +0100

    deplying again

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -3,13 +3,11 @@
 
 import {NextUIProvider} from '@nextui-org/react'
 import { UserProvider } from '@/providers/Context/UserContext'
-import { useRouter } from 'next/navigation';
 
 export function Providers({children}: { children: React.ReactNode }) {
-  const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
+    <NextUIProvider>
       <UserProvider>
       {children}
       </UserProvider>
