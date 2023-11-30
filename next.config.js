b15285a5d95f63245df7bac6b78dc27a446dcc39
Jakub Svoboda <132791205+jacobfreedom@users.noreply.github.com>
2023-11-30 23:05:08 +0100

    Deploying

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-const path = require('path')
-
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
 }
+ 
+module.exports = nextConfig
