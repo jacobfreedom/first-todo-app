a2979441566d68d7f9d09b966d446e7bc0725adc
Jakub Svoboda <132791205+jacobfreedom@users.noreply.github.com>
2023-11-30 23:43:14 +0100

    deplyoment test 2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  /* config options here */
+export default (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+  }
+  return nextConfig
 }
- 
-module.exports = nextConfig
