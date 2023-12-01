1b8cb55a144b8d305f64079618f38c0c88814ea3
Jakub Svoboda <132791205+jacobfreedom@users.noreply.github.com>
2023-12-01 09:23:05 +0100

    mobile zoom 8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
-  userScalable: false,
+  userScalable: true,
 };
 
 export default function RootLayout({
