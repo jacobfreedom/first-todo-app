34d69db1349cad37628eff9aba591dfbfbc93c96
Jakub Svoboda <132791205+jacobfreedom@users.noreply.github.com>
2023-12-01 07:15:04 +0100

    mobile 6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { Providers } from "@/providers/providers";
 export const metadata: Metadata = {
   title: "Todo App by Jakub",
   description: "First App Ever",
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
-import Head from 'next/head';
 import Nav from '@/components/NavigationBar';
 import TodoInterface from '@/components/TodoInterface/TodoInterface';
 import { TaskProvider } from '@/providers/Context/TaskContext';
 
 export default function Home() {
+
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
-      </Head>
-      <main className="flex flex-col mx-auto my-0 w-full">
-        <Nav />
-        <TaskProvider>
-          <TodoInterface />
-        </TaskProvider>
-      </main>
-    </>
-  );
+    <main className="flex flex-col mx-auto my-0 w-full scale-0">
+      <Nav />
+      <TaskProvider>
+        <TodoInterface />
+      </TaskProvider>
+    </main>
+  )
 }
