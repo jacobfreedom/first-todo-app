fe57fc05dc478a4389483223710f7c914150a853
Jakub Svoboda <132791205+jacobfreedom@users.noreply.github.com>
2023-12-01 06:16:18 +0100

    mobile zoom fix 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" user-scalable="no">
+    <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
+        />
+      </Head>
       <body>
         <Providers>{children}</Providers>
       </body>
